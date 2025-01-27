08e70fe77efe19196c1a43ebc60bc7dc6a0ac7d2
shakirth-anisha <shakirthanisha@gmail.com>
2025-01-27 08:11:42 +0530

    tailwind thingy

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./layout.js",
+    "./page.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
